97345188b8e2a4ca36e78a8bcbd942736cfd3fe8
zaairaag <zaira.goncalves@ymail.com>
2025-07-19 21:13:01 -0300

    feat: estrutura base do backend Node.js/TypeScript
    - Configuração TypeScript com strict mode
    - Setup Jest para testes automatizados
    - Dockerfile otimizado para desenvolvimento
    - Dependências essenciais: Express, Prisma, JWT, bcrypt
    - Configuração ESLint e Prettier para qualidade de código

---
+++ backend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  env: {
+    node: true,
+    jest: true,
+  },
+};
